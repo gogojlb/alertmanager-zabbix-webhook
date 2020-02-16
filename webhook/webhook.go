cac60d19d2993b6a04f04b44771f8839cbce2d13
Maxim Korolev <gogojlb@mail.ru>
2020-02-16 22:13:29 +0300

    changed to moira webhook

--- webhook/webhook.go
+++ webhook/webhook.go
@@ -17,45 +17,72 @@ import (
 var log = logrus.WithField("context", "webhook")
 
 type WebHook struct {
-	channel chan *Alert
+	channel chan *Event
 	config  WebHookConfig
 }
 
 type WebHookConfig struct {
-	Port                 int    `yaml:"port"`
-	QueueCapacity        int    `yaml:"queueCapacity"`
-	ZabbixServerHost     string `yaml:"zabbixServerHost"`
-	ZabbixServerPort     int    `yaml:"zabbixServerPort"`
-	ZabbixHostDefault    string `yaml:"zabbixHostDefault"`
-	ZabbixHostAnnotation string `yaml:"zabbixHostAnnotation"`
-	ZabbixKeyPrefix      string `yaml:"zabbixKeyPrefix"`
+	Port          int          `yaml:"port"`
+	QueueCapacity int          `yaml:"queueCapacity"`
+	ZabbixConfig  ZabbixConfig `yaml:"zabbix"`
+}
+
+type ZabbixConfig struct {
+	Host      string `yaml:"Host"`
+	Port      int    `yaml:"Port"`
+	HostNodes []int  `yaml:"HostNodes"`
 }
 
 type HookRequest struct {
-	Version           string            `json:"version"`
-	GroupKey          string            `json:"groupKey"`
-	Status            string            `json:"status"`
-	Receiver          string            `json:"receiver"`
-	GroupLabels       map[string]string `json:"groupLabels"`
-	CommonLabels      map[string]string `json:"commonLabels"`
-	CommonAnnotations map[string]string `json:"commonAnnotations"`
-	ExternalURL       string            `json:"externalURL"`
-	Alerts            []Alert           `json:"alerts"`
+	Trigger struct {
+		ID          string   `json:"id"`
+		Name        string   `json:"name"`
+		Description string   `json:"description"`
+		Tags        []string `json:"tags"`
+	} `json:"trigger"`
+	Events  []Event `json:"events"`
+	Contact struct {
+		Type  string `json:"type"`
+		Value string `json:"value"`
+		ID    string `json:"id"`
+		User  string `json:"user"`
+	} `json:"contact"`
+	Plot      string `json:"plot"`
+	Throttled bool   `json:"throttled"`
+}
+type Event struct {
+	Metric       string `json:"metric"`
+	Value        int    `json:"value"`
+	Timestamp    int    `json:"timestamp"`
+	TriggerEvent bool   `json:"trigger_event"`
+	State        string `json:"state"`
+	OldState     string `json:"old_state"`
 }
 
-type Alert struct {
-	Status       string            `json:"status"`
-	Labels       map[string]string `json:"labels"`
-	Annotations  map[string]string `json:"annotations"`
-	StartsAt     string            `json:"startsAt,omitempty"`
-	EndsAt       string            `json:"endsAt,omitempty"`
-	GeneratorURL string            `json:"generatorURL"`
-}
+// type HookRequest struct {
+// 	Version           string            `json:"version"`
+// 	GroupKey          string            `json:"groupKey"`
+// 	Status            string            `json:"status"`
+// 	Receiver          string            `json:"receiver"`
+// 	GroupLabels       map[string]string `json:"groupLabels"`
+// 	CommonLabels      map[string]string `json:"commonLabels"`
+// 	CommonAnnotations map[string]string `json:"commonAnnotations"`
+// 	ExternalURL       string            `json:"externalURL"`
+// 	Alerts            []Alert           `json:"alerts"`
+// }
+
+// type Alert struct {
+// 	Status       string            `json:"status"`
+// 	Labels       map[string]string `json:"labels"`
+// 	Annotations  map[string]string `json:"annotations"`
+// 	StartsAt     string            `json:"startsAt,omitempty"`
+// 	EndsAt       string            `json:"endsAt,omitempty"`
+// 	GeneratorURL string            `json:"generatorURL"`
+// }
 
 func New(cfg *WebHookConfig) *WebHook {
-
 	return &WebHook{
-		channel: make(chan *Alert, cfg.QueueCapacity),
+		channel: make(chan *Event, cfg.QueueCapacity),
 		config:  *cfg,
 	}
 }
@@ -69,13 +96,13 @@ func ConfigFromFile(filename string) (cfg *WebHookConfig, err error) {
 
 	// Default values
 	config := WebHookConfig{
-		Port:                 8080,
-		QueueCapacity:        500,
-		ZabbixServerHost:     "127.0.0.1",
-		ZabbixServerPort:     10051,
-		ZabbixHostAnnotation: "zabbix_host",
-		ZabbixKeyPrefix:      "prometheus",
-		ZabbixHostDefault:    "",
+		Port:          8080,
+		QueueCapacity: 500,
+		ZabbixConfig: ZabbixConfig{
+			Host:      "127.0.0.1",
+			Port:      10051,
+			HostNodes: []int{0, 1},
+		},
 	}
 
 	err = yaml.Unmarshal(configFile, &config)
@@ -90,7 +117,7 @@ func ConfigFromFile(filename string) (cfg *WebHookConfig, err error) {
 func (hook *WebHook) Start() error {
 
 	// Launch the process thread
-	go hook.processAlerts()
+	go hook.processEvents()
 
 	// Launch the listening thread
 	log.Println("Initializing HTTP server")
@@ -126,47 +153,63 @@ func (hook *WebHook) postHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "request body is not valid json", 400)
 		return
 	}
-
-	for index := range m.Alerts {
-		hook.channel <- &m.Alerts[index]
+	var alertEvents []Event
+	for eventIndex := range m.Events {
+		if m.Events[eventIndex].TriggerEvent == true {
+			alertEvents = append(alertEvents, m.Events[eventIndex])
+		}
+	}
+	for index := range alertEvents {
+		hook.channel <- &alertEvents[index]
 	}
 
 }
 
-func (hook *WebHook) processAlerts() {
+func (hook *WebHook) processEvents() {
 
 	log.Info("Alerts queue started")
 
 	// While there are alerts in the queue, batch them and send them over to Zabbix
-	var metrics []*zabbix.Metric
+	var zabbixMetrics []*zabbix.Metric
 	for {
 		select {
-		case a := <-hook.channel:
-			if a == nil {
+		case e := <-hook.channel:
+			if e == nil {
 				log.Info("Queue Closed")
 				return
 			}
+			var moiraMetrics []string
+			var hostNodes []string
+			moiraMetrics = strings.Split(e.Metric, ".")
 
-			host, exists := a.Annotations[hook.config.ZabbixHostAnnotation]
-			if !exists {
-				host = hook.config.ZabbixHostDefault
+			for _, moiraHostNode := range hook.config.ZabbixConfig.HostNodes {
+				hostNodes = append(hostNodes, moiraMetrics[moiraHostNode])
 			}
+			host := strings.Join(hostNodes, " ")
+
+			// host, exists := a.Annotations[hook.config.ZabbixHostAnnotation]
+			// if !exists {
+			// 	host = hook.config.ZabbixHostDefault
+			// }
 
 			// Send alerts only if a host annotation is present or configuration for default host is not empty
 			if host != "" {
-				key := fmt.Sprintf("%s.%s", hook.config.ZabbixKeyPrefix, strings.ToLower(a.Labels["alertname"]))
+				key := fmt.Sprintf("%s", e.Metric)
 				value := "0"
-				if a.Status == "firing" {
+				if e.State == "ERROR" {
+					value = "2"
+				}
+				if e.State == "WARN" {
 					value = "1"
 				}
 
 				log.Infof("added Zabbix metrics, host: '%s' key: '%s', value: '%s'", host, key, value)
-				metrics = append(metrics, zabbix.NewMetric(host, key, value))
+				zabbixMetrics = append(zabbixMetrics, zabbix.NewMetric(host, key, value))
 			}
 		default:
-			if len(metrics) != 0 {
-				hook.zabbixSend(metrics)
-				metrics = metrics[:0]
+			if len(zabbixMetrics) != 0 {
+				hook.zabbixSend(zabbixMetrics)
+				zabbixMetrics = zabbixMetrics[:0]
 			} else {
 				time.Sleep(1 * time.Second)
 			}
@@ -179,8 +222,8 @@ func (hook *WebHook) zabbixSend(metrics []*zabbix.Metric) {
 	packet := zabbix.NewPacket(metrics)
 
 	// Send packet to zabbix
-	log.Infof("sending to zabbix '%s:%d'", hook.config.ZabbixServerHost, hook.config.ZabbixServerPort)
-	z := zabbix.NewSender(hook.config.ZabbixServerHost, hook.config.ZabbixServerPort)
+	log.Infof("sending to zabbix '%s:%d'", hook.config.ZabbixConfig.Host, hook.config.ZabbixConfig.Port)
+	z := zabbix.NewSender(hook.config.ZabbixConfig.Host, hook.config.ZabbixConfig.Port)
 	_, err := z.Send(packet)
 	if err != nil {
 		log.Error(err)
